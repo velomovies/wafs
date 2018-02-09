4251dd65f6fc547fd3d2aa455d61c4cd3baf8f61
Jelle Overbeek <j.overbeek@recognize.nl>
2018-02-09 10:11:20 +0100

    Removed unnessesary code and added translateZ to improve performance

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -3,19 +3,15 @@
 
     var app = {
         init: function () {
-            this.handleEvents()
+            var hash = location.hash
+            if (hash) {
+                sections.toggle(hash)
+            } else {
+                sections.toggle('#start')
+            }
+
             routes.init()
-        },
-        handleEvents: function () {
-            window.addEventListener('load', function (event) {
-                var hash = location.hash
-                if (hash) {
-                    routes.pushState(hash)
-                } else {
-                    sections.toggle('#start')
-                }
-            })
-        }    
+        }
     }
 
     var routes = {
@@ -23,22 +19,21 @@
             this.handleEvents()
         }, 
         handleEvents: function () {
-            var self = this
             window.addEventListener('hashchange', function (event) {
                 var hash = location.hash
-                self.pushState(hash)
+                sections.toggle(hash)
             })
-        },
-        pushState: function (route) {
-            sections.toggle(route)
         }
     }
 
     var sections = {
         toggle: function (route) {
-            document.querySelectorAll('section').forEach(function (el) {
+            var sections = document.querySelectorAll('section')
+
+            sections.forEach(function (el) {
                 el.classList.remove('active')
             })
+
             document.querySelector(route).classList.add('active')
         }
     }
