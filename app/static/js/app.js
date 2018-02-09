a37f7959cb41d727c15df0ed5092429567190b0d
Vienna <viennameijer@gmail.com>
2018-02-09 13:39:37 +0100

    remove helper functions

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,15 +1,6 @@
 (function () {
     "use strict"
 
-    var helper = {
-        qs: function (el) {
-            return document.querySelector(el)
-        },
-        qsa: function (el) {
-            return document.querySelectorAll(el)
-        }
-    }
-
     var app = {
         init: function () {
             this.handleEvents()
@@ -45,24 +36,10 @@
 
     var sections = {
         toggle: function (route) {
-<<<<<<< HEAD
-            helper.qsa('section').forEach(function (el) {
+            document.querySelectorAll('section').forEach(function (el) {
                 el.classList.remove('active')
-            })
-            helper.qs(route).classList.add('active')
-=======
-            var a = document.querySelectorAll('a')
-            var aActive = document.querySelectorAll('[href="' + route + '"]')
-            var sections = document.querySelectorAll('section')
-            sections.forEach(function (el) {
-                el.classList.remove('active')
-            })
-            a.forEach(function (a) {
-                a.classList.remove('active')
             })
             document.querySelector(route).classList.add('active')
-            aActive[0].classList.add('active')
->>>>>>> 99da6ec3eb69935778ade7a0ca9d7d080269d5c1
         }
     }
 
