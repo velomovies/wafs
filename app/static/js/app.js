99da6ec3eb69935778ade7a0ca9d7d080269d5c1
Victor Zumpolle <victor@victorzumpolle.nl>
2018-02-09 13:35:15 +0100

    Update files

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -36,11 +36,17 @@
 
     var sections = {
         toggle: function (route) {
+            var a = document.querySelectorAll('a')
+            var aActive = document.querySelectorAll('[href="' + route + '"]')
             var sections = document.querySelectorAll('section')
             sections.forEach(function (el) {
                 el.classList.remove('active')
             })
+            a.forEach(function (a) {
+                a.classList.remove('active')
+            })
             document.querySelector(route).classList.add('active')
+            aActive[0].classList.add('active')
         }
     }
 
