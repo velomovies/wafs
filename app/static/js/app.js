f5df6b565998b8025529a3e7fdf7e20378c318c5
Victor Zumpolle <victor@victorzumpolle.nl>
2018-02-16 11:16:41 +0100

    Added style and some comments to JS

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,18 +1,26 @@
+// Uses routie by Greg Allen (http://projects.jga.me/routie/) for routing 
+// &
+// Uses transparancy by Leonidas (http://leonidas.github.com/transparency/) for templating
+
 (function () {
   "use strict"
 
   var app = {
     init: function () {
+      // Initialize the app and start router
       router.init()
     }
   }
 
   var router = {
     init: function () { 
+      // Checks with routie which section it has to show
       routie({
         'home': function () {
+          // Toggles section and gets data from api. Parameter for the right data endpoint
           sections.toggle('#home')
           api.getData('/pages' + api.language + '/rijksstudio/kunstenaars/' + api.artistInfo + '?').then(function (data) {
+            // Renders the right data with a few parameters to show the right content
             template.render(data, '#home', template.home)
           }) 
         },
@@ -28,9 +36,11 @@
             template.render(data, '#detail', template.detail)
           })
         },
+        // Routes automatically to home when there is no #
         '': function () {
-          sections.toggle('#home')
+          routie('home')
         },
+        // Fallback when the # is different than above
         '*': function () {
           sections.toggle('#error')
         }
@@ -39,6 +49,7 @@
   }
 
   var api = {
+    // A config for showing the right data
     url: 'https://www.rijksmuseum.nl/api',
     format: 'json',
     artistCollection: 'Rembrandt+van+Rijn',
@@ -50,6 +61,7 @@
     imageOnly: true,
     language: '/nl',
 
+    // Gets data from api with an url that is specified above and in the route
     getData: function (dataEndpoint) {
       var loadData = new Promise(function (resolve, reject) {
         var request = new XMLHttpRequest();
@@ -57,6 +69,7 @@
     
         request.onload = function () {
           if (request.status >= 200 && request.status < 400) {
+            // Gets data and resolves and rejects if error
             var data = JSON.parse(request.responseText);
             resolve(data)
           } else {
@@ -70,17 +83,18 @@
     
         request.send()
       })
-      
+      // It returns loadData so i can make a 'then' immediately in the route
       return loadData;
     }
   }
 
   var sections = {
+    // Toggles the right section and adds active states
     toggle: function (route) {
       var activeElements = helper.qsa('.active')
       var activeLinkElements = helper.qsa('nav [href="' + route + '"]')
       var sectionElements = helper.qs(route)
-
+      // Turns active links off and adds the right active state
       helper.turnOff(activeElements)
       activeLinkElements.length > 0 && activeLinkElements[0].classList.add('active')
       sectionElements.classList.add('active')
@@ -88,9 +102,12 @@
   }
 
   var template = {
+    // Renders the template in the HTML page
     render: function (data, route, template) {
+      // Gets the render and adds the parameters added in the call
       Transparency.render(helper.qs(route), template(data), this.directivesMethod())
     },
+    // Shows the right data for the right page
     home: function (data) {
       return {
         name: data.contentPage.title,
@@ -114,6 +131,7 @@
         imageUrl: data.artObject.webImage.url
       }
     },
+    // This gets all images and or links and adds the right url to it. Only when it finds it on the page
     directivesMethod: function () {
       var directives = {
         image: {
@@ -133,6 +151,7 @@
 
 
   var helper = {
+    // An helper to instantly remove any active classes on elements and a smaller way to use querySelector and querySelectorAll
     turnOff: function (elements) {
       elements.forEach(function (el) {
         el.classList.remove('active')
